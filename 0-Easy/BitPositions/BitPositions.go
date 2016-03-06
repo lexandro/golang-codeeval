26a97582b8b23bfd3f92b355e16c79d57be228ac
lexandro <lexandro2000@gmail.com>
2016-03-06 20:47:44 +0100

    Solving Bit Positions easy challenge

---
+++ 0-Easy/BitPositions/BitPositions.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ CodeEval's Bit Positions easy challenge solution
+
+ Details: https://www.codeeval.com/open_challenges/19/
+
+ Author: Robert Stern (lexandro2000@gmail.com)
+ Github: https://github.com/lexandro/golang-codeeval
+*/
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		solveChallenge(scanner.Text())
+	}
+}
+
+func solveChallenge(text string) {
+	elements := strings.Split(text, ",")
+	number, _ := strconv.Atoi(elements[0])
+	bit1, _ := strconv.Atoi(elements[1])
+	bit2, _ := strconv.Atoi(elements[2])
+
+	fmt.Println(hasBit(number, uint(bit1)) == hasBit(number, uint(bit2)))
+}
+
+func hasBit(n int, pos uint) bool {
+	val := n & (1 << (pos - 1))
+	return (val > 0)
+}
